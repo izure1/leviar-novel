80f594e9e48cfda98d7296db046517aa019a5583
izure <izure@naver.com>
2026-05-14 19:29:37 +0900

    chore: 프로젝트 파일 정리

--- src/types/index.ts
+++
@@ -1,8 +0,0 @@
-// =============================================================
-// types/index.ts — 타입 재수출
-// =============================================================
-
-export type * from './config'
-export type * from './dialogue'
-export type { VarResolvable } from './utils'
-export { resolveVarResolvable } from './utils'
